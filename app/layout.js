6f6fdf0075ec0220e3dd4c8bf5153663411f335f
khalid-dev <ka1239996@gmail.com>
2025-07-01 13:07:08 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,16 @@ const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] })
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '700'] })
 
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* PWA manifest & icon links */}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="theme-color" content="#0275d8" />
+      </head>
       <body className={`${poppins.className} ${roboto.className}`}>
         {children}
       </body>
